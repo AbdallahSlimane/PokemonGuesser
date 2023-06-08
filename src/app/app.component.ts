0be3b27124033778269c14d724235223d7781833
melis <melissanoemi.laurent@free.fr>
2023-06-08 22:32:59 +0200

    add json server and service #WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PokemonService} from "./services/pokemon.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pokemon-test';
+export class AppComponent implements OnInit, OnDestroy{
+
+  private pokemonSubscription: Subscription | undefined;
+  constructor(private pokemonService: PokemonService) {}
+
+  ngOnInit(): void {
+       this.getPokemonList();
+    }
+
+  getPokemonList() {
+    this.pokemonSubscription = this.pokemonService.getPokemonList().subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    if(this.pokemonSubscription){
+      this.pokemonSubscription.unsubscribe();
+
+    }
+  }
 }
---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Pokemon} from "../interfaces/pokemon.interface";
+import {map} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  constructor(private http: HttpClient) { }
+
+  getPokemonList(){
+    return this.http.get<Pokemon[]>('http://localhost:3000/pokemon').pipe(
+      map(response => response.map(pokemon => ({number: pokemon.number, name: pokemon.name})))
+    );
+  }
+}
